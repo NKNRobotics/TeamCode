a1bee6ee58d4a1ad8ae7f54d3d2f502737ec6351
dmhum <lilshummel@gmail.org>
2025-09-09 17:56:17 -0700

    April tag POC

--- TeamCode/src/main/java/org/nknsd/teamcode/components/sensors/AprilTag.java
+++ TeamCode/src/main/java/org/nknsd/teamcode/components/sensors/AprilTag.java
@@ -22,6 +22,10 @@ public class AprilTag implements NKNComponent {
         limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
         limelight.start(); // This tells Limelight to start looking!
         limelight.pipelineSwitch(2); // Switch to aprilTag pipeline
+
+//        Pipeline 2 is PGP
+//        Pipeline 3 is GPP
+//        Pipeline 4 is PPG
         return true;
     }
 
@@ -49,20 +53,54 @@ public class AprilTag implements NKNComponent {
     double ty; // How far up or down the target is (degrees)
     double ta; // How big the target looks (0%-100% of the image)
 
+    int pattern; // this should probably be an enum, it tells what pattern we have, 4 is PPG, 2 is PGP, 3 is GPP, 1 is nothing;
+
     @Override
     public void loop(ElapsedTime runtime, Telemetry telemetry) {
         LLResult result = limelight.getLatestResult();
+        limelight.pipelineSwitch(2);
         if (result != null && result.isValid()) {
-             tx = result.getTx();
-             ty = result.getTy();
-             ta = result.getTa();
+            pattern = 2;
+        } else {
+            limelight.pipelineSwitch(3);
+            if (result != null && result.isValid()) {
+                pattern = 3;
+            } else {
+                limelight.pipelineSwitch(4);
+                if (result != null && result.isValid()) {
+                    pattern = 4;
+                } else {
+                    pattern = 1;
+                    ta = 0;
+                }
+            }
+
+        }
+
+        if (pattern != 1) {
+            tx = result.getTx();
+            ty = result.getTy();
+            ta = result.getTa();
         }
     }
 
     @Override
     public void doTelemetry(Telemetry telemetry) {
-        telemetry.addData("Target X", tx);
-        telemetry.addData("Target Y", ty);
-        telemetry.addData("Target Area", ta);
+        if (pattern == 1) {
+            telemetry.addLine("Not seen");
+        } else {
+            telemetry.addData("Target X", tx);
+            telemetry.addData("Target Y", ty);
+            telemetry.addData("Target Area", ta);
+
+            if(pattern == 2){
+                telemetry.addLine("PGP");
+            } else if(pattern == 3){
+                telemetry.addLine("GPP");
+            } else{
+                telemetry.addLine("PPG");
+            }
+        }
+
     }
 }
--- TeamCode/src/main/java/org/nknsd/teamcode/programs/tests/AprilTester.java
+++ TeamCode/src/main/java/org/nknsd/teamcode/programs/tests/AprilTester.java
@@ -8,9 +8,8 @@ import org.nknsd.teamcode.frameworks.NKNComponent;
 import org.nknsd.teamcode.frameworks.NKNProgram;
 
 import java.util.List;
-
+@TeleOp(name = "AprilTagTester", group="Tests")
 public class AprilTester extends NKNProgram {
-    @TeleOp(name = "Advanced Movement Test", group="Tests")
     public void createComponents(List<NKNComponent> components, List<NKNComponent> telemetryEnabled) {
         AprilTag aprilTag = new AprilTag();
         components.add(aprilTag);
