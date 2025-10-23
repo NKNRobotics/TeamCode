318424abb6ded0c18b0ae9a99e1b47e8543eb5a8
AlexKing11 <dillonqgardner@gmail.com>
2025-10-23 11:59:39 -0700

    Improved the aesthetics of the MicrowaveTeleOp for readability. And added Intake stuff to it, despite linking functions not existing.

--- TeamCode/src/main/java/org/nknsd/teamcode/programs/teleops/MicrowaveTeleOp.java
+++ TeamCode/src/main/java/org/nknsd/teamcode/programs/teleops/MicrowaveTeleOp.java
@@ -2,8 +2,10 @@ package org.nknsd.teamcode.programs.teleops;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.nknsd.teamcode.components.drivers.IntakeDriver;
 import org.nknsd.teamcode.components.drivers.MicrowaveDriver;
 import org.nknsd.teamcode.components.drivers.WheelDriver;
+import org.nknsd.teamcode.components.handlers.IntakeHandler;
 import org.nknsd.teamcode.components.handlers.MicrowaveHandler;
 import org.nknsd.teamcode.components.handlers.WheelHandler;
 import org.nknsd.teamcode.components.sensors.ColourSensor;
@@ -18,32 +20,51 @@ import java.util.List;
 public class MicrowaveTeleOp extends NKNProgram {
     @Override
     public void createComponents(List<NKNComponent> components, List<NKNComponent> telemetryEnabled) {
-        WheelHandler wheelHandler = new WheelHandler();
-        components.add(wheelHandler);
-
+        // SPECIAL
         GamePadHandler gamePadHandler = new GamePadHandler();
         components.add(gamePadHandler);
 
-        WheelDriver wheelDriver = new WheelDriver(0.1, 1, 3, GamePadHandler.GamepadSticks.LEFT_JOYSTICK_Y, GamePadHandler.GamepadSticks.LEFT_JOYSTICK_X, GamePadHandler.GamepadSticks.RIGHT_JOYSTICK_X);
+
+        // WHEELS
+        WheelHandler wheelHandler = new WheelHandler();
+        components.add(wheelHandler);
+
+        WheelDriver wheelDriver = new WheelDriver(0, 1, 5, GamePadHandler.GamepadSticks.LEFT_JOYSTICK_Y, GamePadHandler.GamepadSticks.LEFT_JOYSTICK_X, GamePadHandler.GamepadSticks.RIGHT_JOYSTICK_X);
         components.add(wheelDriver);
 
-        WheelControlScheme wheelControlScheme = new WheelControlScheme();
-        wheelControlScheme.link(gamePadHandler);
-        wheelDriver.link(gamePadHandler,wheelHandler,wheelControlScheme);
 
-        //setup for the microwave
+        // SENSORS
 //        ColourSensor colourSensor = new ColourSensor("ColorSensor");
 //        components.add(colourSensor);
 
+
+        // MICROWAVE
         MicrowaveHandler microwaveHandler = new MicrowaveHandler("Spin");
-//        microwaveHandler.link(colourSensor);
         components.add(microwaveHandler);
 
         MicrowaveDriver microwaveDriver = new MicrowaveDriver();
         components.add(microwaveHandler);
 
+
+        // INTAKE
+        IntakeHandler intakeHandler = new IntakeHandler();
+        components.add(intakeHandler);
+
+        IntakeDriver intakeDriver = new IntakeDriver();
+        components.add(intakeDriver);
+
+
+        // CONTROL SCHEME
+        WheelControlScheme wheelControlScheme = new WheelControlScheme();
+        wheelControlScheme.link(gamePadHandler);
+
         MicrowaveControlScheme microwaveControlScheme = new MicrowaveControlScheme();
-        microwaveDriver.link(gamePadHandler, microwaveHandler, microwaveControlScheme);
         microwaveControlScheme.link(gamePadHandler);
+
+
+        // LINK
+//        microwaveHandler.link(colourSensor);
+        wheelDriver.link(gamePadHandler,wheelHandler,wheelControlScheme);
+        microwaveDriver.link(gamePadHandler, microwaveHandler, microwaveControlScheme);
     }
 }
