c4f4f6180455943fb6c9be7018bcf0013f253563
AlexKing11 <dillonqgardner@gmail.com>
2025-10-23 11:48:44 -0700

    Created a basic intakeHandler class (no code yet, but has framework)

---
+++ TeamCode/src/main/java/org/nknsd/teamcode/components/handlers/IntakeHandler.java
@@ -0,0 +1,54 @@
+package org.nknsd.teamcode.components.handlers;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.internal.webserver.websockets.CommandNotImplementedException;
+import org.nknsd.teamcode.frameworks.NKNComponent;
+
+public class IntakeHandler implements NKNComponent {
+    @Override
+    public boolean init(Telemetry telemetry, HardwareMap hardwareMap, Gamepad gamepad1, Gamepad gamepad2) {
+        return true;
+    }
+
+    @Override
+    public void init_loop(ElapsedTime runtime, Telemetry telemetry) {
+
+    }
+
+    @Override
+    public void start(ElapsedTime runtime, Telemetry telemetry) {
+
+    }
+
+    @Override
+    public void stop(ElapsedTime runtime, Telemetry telemetry) {
+
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public void loop(ElapsedTime runtime, Telemetry telemetry) {
+
+    }
+
+    @Override
+    public void doTelemetry(Telemetry telemetry) {
+
+    }
+
+    public void toggleIntake(boolean startSpinning) {
+
+    }
+
+    public boolean isSpinning() {
+        return false;
+    }
+}
