e290e6d5cdf54ef391e6820c39fda66d9f865caf
Kermes05 <kmj.johansen@gmail.com>
2025-12-08 16:55:56 -0800

    message

--- TeamCode/src/main/java/org/nknsd/teamcode/autoStates/AutoReadPatternState.java
+++ TeamCode/src/main/java/org/nknsd/teamcode/autoStates/AutoReadPatternState.java
@@ -3,12 +3,13 @@ package org.nknsd.teamcode.autoStates;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.nknsd.teamcode.components.handlers.vision.ID;
 import org.nknsd.teamcode.components.sensors.AprilTagSensor;
 import org.nknsd.teamcode.components.utility.StateMachine;
 
 public class AutoReadPatternState extends StateMachine.State {
     private final AprilTagSensor aprilTagSensor;
-
+    private ID pattern;
     public AutoReadPatternState(AprilTagSensor aprilTagSensor) {
         this.aprilTagSensor = aprilTagSensor;
     }
@@ -20,7 +21,7 @@ public class AutoReadPatternState extends StateMachine.State {
 
     @Override
     protected void started() {
-        pattern = aprilTagSensor.getVisionResultPattern();
+        pattern = aprilTagSensor.getVisionResultPattern().id;
     }
 
     @Override
