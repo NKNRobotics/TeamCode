4999a6da0d70495aebf3acad25af91f0b627f56d
Kermes05 <kmj.johansen@gmail.com>
2026-01-05 21:47:18 -0800

    detectingBallCode

---
+++ TeamCode/src/main/java/org/nknsd/teamcode/programs/tests/allYears/sensor/SRSPeakFindingTester.java
@@ -0,0 +1,13 @@
+package org.nknsd.teamcode.programs.tests.allYears.sensor;
+
+import org.nknsd.teamcode.frameworks.NKNComponent;
+import org.nknsd.teamcode.frameworks.NKNProgram;
+
+import java.util.List;
+
+public class SRSPeakFindingTester extends NKNProgram {
+    @Override
+    public void createComponents(List<NKNComponent> components, List<NKNComponent> telemetryEnabled) {
+
+    }
+}
