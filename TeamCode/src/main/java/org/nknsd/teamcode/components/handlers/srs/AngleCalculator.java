b662864d345b8187a6b99bf1784efbef361be35a
AlexKing11 <dillonqgardner@gmail.com>
2026-01-26 17:22:47 -0800

    fixed math

--- TeamCode/src/main/java/org/nknsd/teamcode/components/handlers/srs/AngleCalculator.java
+++ TeamCode/src/main/java/org/nknsd/teamcode/components/handlers/srs/AngleCalculator.java
@@ -3,13 +3,16 @@ package org.nknsd.teamcode.components.handlers.srs;
 public class AngleCalculator {
     // Represents the distance between the sensor and the center of the robot in cm. I think it's cm. Whatever the sensor measures distance in.
     private static final double sensorOffset = 0;
+    private static final double rightAngle = 3.14159 / 2;
+    private static final double angleBetweenDataPoints = (0.785398 / 7);
+    private static final double halfOfFov = 0.3926991;
 
     public static double calculateHeadingOffset(int ballXPos, int ballDist) {
-        double angleFromPlane = 90 + (45.0 / 7) * ballXPos - 22.5;
+        double angleFromPlane = rightAngle + angleBetweenDataPoints * ballXPos - halfOfFov;
         double distFromOriginToBall = (sensorOffset * sensorOffset) + (ballDist * ballDist)
                 - (2 * sensorOffset * ballDist * Math.cos(angleFromPlane));
-        double headingOffset = 90 - Math.asin(
-                Math.sin(angleFromPlane) * (sensorOffset / distFromOriginToBall)
+        double headingOffset = rightAngle - Math.asin(
+                Math.sin(angleFromPlane) * (ballDist / distFromOriginToBall)
         );
 
         return headingOffset;
