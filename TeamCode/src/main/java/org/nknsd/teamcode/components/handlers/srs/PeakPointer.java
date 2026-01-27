1258cd9d7c6168f64716a61effc00dcc439b026f
Kermes05 <kmj.johansen@gmail.com>
2026-01-27 10:09:50 -0800

    a

--- TeamCode/src/main/java/org/nknsd/teamcode/components/handlers/srs/PeakPointer.java
+++ TeamCode/src/main/java/org/nknsd/teamcode/components/handlers/srs/PeakPointer.java
@@ -1,23 +1,31 @@
 package org.nknsd.teamcode.components.handlers.srs;
 
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.nknsd.teamcode.components.handlers.odometry.AbsolutePosition;
+import org.nknsd.teamcode.components.motormixers.AutoPositioner;
 import org.nknsd.teamcode.components.motormixers.PowerInputMixer;
 import org.nknsd.teamcode.components.utility.IntPoint;
 import org.nknsd.teamcode.components.utility.StateMachine;
+import org.nknsd.teamcode.components.utility.feedbackcontroller.PidController;
+import org.nknsd.teamcode.frameworks.NKNComponent;
 
 import java.util.Objects;
 
-public class PeakPointer {
+public class PeakPointer implements NKNComponent {
     private final PeakFinder peakFinder;
-    private final SRSHubHandler srsHubHandler;
-    private final PowerInputMixer inputMixer;
+    private AutoPositioner positioner;
     private final AbsolutePosition absPos;
+    private final SRSHubHandler srsHubHandler;
 
     public boolean targetPeaks(){
-        IntPoint ballLocation = peakFinder.altPeakFind(srsHubHandler.getNormalizedDists());
-        if(!ballLocation.equals(new IntPoint(-10, -10))) {
-            StateMachine.INSTANCE.startAnonymous(new PeakTargetState(ballLocation, inputMixer, absPos));
-            return true;
+        IntPoint currentPeak = new IntPoint(peakFinder.getPeak(srsHubHandler.getNormalizedDists()).getX(), peakFinder.getPeak(srsHubHandler.getNormalizedDists()).getY());
+        if(peakFinder.getPeak(srsHubHandler.getNormalizedDists()).getX() != 10){
+            positioner.enableAutoPositioning(true);
+//            positioner.setTargetH(AngleCalculator.calculateHeadingOffset(currentPeak.getX(), srsHubHandler.getDistances()[currentPeak.getX()][currentPeak.getY()]), new PidController());
         }
         return false;
     }
@@ -25,10 +33,48 @@ public class PeakPointer {
 
     }
 
-    public PeakPointer(PeakFinder peakFinder, SRSHubHandler srsHubHandler, PowerInputMixer inputMixer, AbsolutePosition absPos) {
+    public PeakPointer(PeakFinder peakFinder, SRSHubHandler srsHubHandler, AbsolutePosition absPos) {
         this.peakFinder = peakFinder;
-        this.srsHubHandler = srsHubHandler;
-        this.inputMixer = inputMixer;
         this.absPos = absPos;
+        this.srsHubHandler = srsHubHandler;
+    }
+
+    @Override
+    public boolean init(Telemetry telemetry, HardwareMap hardwareMap, Gamepad gamepad1, Gamepad gamepad2) {
+        return false;
+    }
+
+    @Override
+    public void init_loop(ElapsedTime runtime, Telemetry telemetry) {
+
+    }
+
+    @Override
+    public void start(ElapsedTime runtime, Telemetry telemetry) {
+
+    }
+
+    @Override
+    public void stop(ElapsedTime runtime, Telemetry telemetry) {
+
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public void loop(ElapsedTime runtime, Telemetry telemetry) {
+
+    }
+
+    @Override
+    public void doTelemetry(Telemetry telemetry) {
+
+    }
+
+    public void link(AutoPositioner positioner){
+        this.positioner = positioner;
     }
 }
