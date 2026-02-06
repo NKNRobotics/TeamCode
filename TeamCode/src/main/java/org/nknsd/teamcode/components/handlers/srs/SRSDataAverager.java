16debb0ebccde202370ce32f6dd4caef4cbbe5d0
dmhum <lilshummel@gmail.org>
2026-02-05 17:36:56 -0800

    Created a framework for a class that automatically averages arrays for SRS hub purposes.

---
+++ TeamCode/src/main/java/org/nknsd/teamcode/components/handlers/srs/SRSDataAverager.java
@@ -0,0 +1,51 @@
+package org.nknsd.teamcode.components.handlers.srs;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.nknsd.teamcode.frameworks.NKNComponent;
+
+import java.util.concurrent.Callable;
+
+public class SRSDataAverager implements NKNComponent {
+    public int[][] getAveragedData() {
+        return null;
+    }
+    
+    @Override
+    public boolean init(Telemetry telemetry, HardwareMap hardwareMap, Gamepad gamepad1, Gamepad gamepad2) {
+        return false;
+    }
+
+    @Override
+    public void init_loop(ElapsedTime runtime, Telemetry telemetry) {
+
+    }
+
+    @Override
+    public void start(ElapsedTime runtime, Telemetry telemetry) {
+
+    }
+
+    @Override
+    public void stop(ElapsedTime runtime, Telemetry telemetry) {
+
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public void loop(ElapsedTime runtime, Telemetry telemetry) {
+
+    }
+
+    @Override
+    public void doTelemetry(Telemetry telemetry) {
+
+    }
+}
